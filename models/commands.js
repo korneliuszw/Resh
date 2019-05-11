ac57ce7f9f0aeb998a63f1026664d9aeacbf7776
kawuka <korneliuszwstudio@gmail.com>
2019-05-12 00:56:45 +0200

    Revert missclick :D

---
+++ models/commands.js
@@ -0,0 +1,60 @@
+const methods = {
+    youtube: require('../commands/youtube.js'),
+    admin: require('../commands/setup.js')
+}
+
+//All commands avaible here. Use to check if message is command and go to the section and then parse it to the method with same name ex. if message starts with *play - go to section youtube(../commands/youtube.js)     
+const commands = {
+    '*play': {
+        help: 'Play music from youtube. Currently only supporting single songs',
+        usage: '*play name|url',
+        func: (msg, song) => {
+            methods.youtube.play(msg, song)
+        }
+    },
+    '*quit': {
+        help: 'Quit voice channel - stop playing music',
+        func: (msg) => {
+            methods.youtube.quit(msg)
+        }
+    },
+    '*skip': {
+        help: 'Skip song - play next in queue',
+        func: (msg) => {
+            methods.youtube.skip(msg)
+        }
+    },
+    '*find': {
+        help: 'Find a video using keywoards',
+        func: (msg, words) => {
+            methods.youtube.find(words, msg, true)
+        }
+    },
+    '*help': {
+        help: 'Get all commands with method of usage',
+        func: async (msg) => {
+            mess = 'Command | Usage\n'
+            Object.entries(commands).forEach((command) => {
+                mess += `** ${command[0]} ** | ${command[1].help} `
+                if (command[1].usage) {
+                    sp = command[1].usage.split(' ')
+                    mess += ` ** ${sp[0]} _${sp[1]}_ ** `
+                }
+                mess += '\n'
+            })
+            msg.author.send(mess)
+
+        }
+    },
+    '*set_channel': {
+        func(msg, channel) {
+
+        }
+    },
+    '*setup': {
+        func: async (msg) => {
+            methods.admin.setup(msg)
+        }
+    }
+}
+module.exports = commands
